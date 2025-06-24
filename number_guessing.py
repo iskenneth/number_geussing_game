b1bdf6824a7163bbe5ed5ed9c3aafa3b42a9e998
iskenneth <serranokenneth94@gmail.com>
2025-06-25 01:45:56 +0800

    typo errors

--- number_guessing.py
+++ number_guessing.py
@@ -9,21 +9,21 @@ class NumberGuessing:
 
         self.number = random.randint(1,10)
 
-        self.label = tk.label(master, text="Input a number from 1-10:")
+        self.label = tk.Label(master, text="Input a number from 1-10:")
         self.label.pack()
         self.entry = tk.Entry(master)
         self.entry.pack()
 
-        self.guess_button = tk.Button(master, text="Prediction", command=self.guess_right)
+        self.guess_button = tk.Button(master, text="Prediction", command=self.guess_checker)
         self.guess_button.pack()
-        self.feedback = tk.label (master, text+"")
+        self.feedback = tk.Label (master, text="")
         self.feedback.pack()
-        self.play_again_button = tk.Button(master, text="Play Again", command=self.play_again)
+        self.play_again_button = tk.Button(master, text="isa_pa", command=self.isa_pa)
         self.play_again_button.pack()
         self.play_again_button.config(state="disabled")
     def guess_checker(self): 
         try:
-            guess + int(self.entry.get())
+            guess = int(self.entry.get())
         except ValueError:
             self.feedback.config(text="Please enter a valid number!")
             return
